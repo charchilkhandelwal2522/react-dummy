369dfef5477aaaab67299f5580274edc3d6db4c0
Charchil Khandelwal <charchilkhandelwalmitrc@gmail.com>
2026-06-11 19:33:25 +0530

    WIP

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-3 fixed-bottom">
+    <footer className="bg-dark text-light border-top py-3 fixed-bottom">
       <p className="text-center">
         Copyright &copy; MyTodosList.com
       </p>
